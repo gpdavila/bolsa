e53d9dea44bb348622c29044a06b407cb7b70e70
gpdavila <gpiscoya2013@hotmail.com>
2017-08-21 11:51:55 -0300

    CEDD

---
+++ CEDD/main.d
@@ -0,0 +1,4 @@
+main.o: main.cpp kernel.h support/common.h support/ocl.h \
+ /opt/AMDAPPSDK-3.0/include/CL/cl.h \
+ /opt/AMDAPPSDK-3.0/include/CL/cl_platform.h support/partitioner.h \
+ support/timer.h support/verify.h support/common.h
